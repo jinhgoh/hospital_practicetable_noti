6590a08871df16712f7c948119b619a5ec7223c2
Jinhyong Goh <68317603+jinhgoh@users.noreply.github.com>
2022-11-22 23:30:13 +0900

    first commit

---
+++ sendSlack.js
@@ -0,0 +1,18 @@
+function sendSlack_range(e) {
+  const log = e.range.getSheet()
+  const cellvalue = SpreadsheetApp.getActiveRange().getValue()
+  const slackUrl = "https://hooks.slack.com/{ID_KEY}"
+  const params = {
+    method : "post",
+    contentType: "application/json",
+    payload: JSON.stringify({
+      "text": "---------------------------------------------------------" + "\n" + " <- "+ cellvalue + "\n 추가날짜: " + new Date()
+    })
+  };
+  if (cellvalue.includes('고진형') || cellvalue.includes(222238) || (cellvalue.includes("Jason") && cellvalue.includes("CT")) || (cellvalue.includes("Peter") && cellvalue.includes("MRI"))) {
+    const sendMsg = UrlFetchApp.fetch(slackUrl, params)
+    const respCode = sendMsg.getResponseCode()
+    Logger.log(sendMsg)
+    Logger.log(respCode)
+  }
+}
